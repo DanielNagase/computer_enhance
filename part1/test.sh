ad0e672726374716a42fe810fef01db89d0c6fba
Daniel Nagase <daniel.nagase@unity3d.com>
2026-05-28 18:56:14 +0900

    Update how executables are run in the test script
    I didn't know this, but it turns out that you need to run the
    executable with the mono command. I was getting
    System.TypeLoadExceptionError errors depending on the content of input
    file. I'd get similar errors when running code that used the Span and
    ReadOnlySpan classes, but this fixes those errors as well.

--- part1/test.sh
+++ part1/test.sh
@@ -2,11 +2,12 @@
 mcs sim.cs
 mkdir -p output
 
-./sim.exe listing_0037_single_register_mov > output/listing_0037_single_register_mov.asm
+# you need to call the exe with the 'mono' command to avoid System.TypeLoadException errors
+mono ./sim.exe listing_0037_single_register_mov > output/listing_0037_single_register_mov.asm
 nasm output/listing_0037_single_register_mov.asm
 diff -q output/listing_0037_single_register_mov listing_0037_single_register_mov
 
-./sim.exe listing_0038_many_register_mov > output/listing_0038_many_register_mov.asm
+mono ./sim.exe listing_0038_many_register_mov > output/listing_0038_many_register_mov.asm
 nasm output/listing_0038_many_register_mov.asm
 diff -q output/listing_0038_many_register_mov listing_0038_many_register_mov
 
