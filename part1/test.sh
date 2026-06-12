d9a81593d7e775c81c425d9f896ec9ff4f7a9635
Daniel Nagase <daniel.nagase@unity3d.com>
2026-06-12 17:13:13 +0900

    Test listing 41 in the test script

--- part1/test.sh
+++ part1/test.sh
@@ -15,3 +15,6 @@ mono ./sim.exe listing_0039_more_movs > output/listing_0039_more_movs.asm
 nasm output/listing_0039_more_movs.asm
 diff -q output/listing_0039_more_movs listing_0039_more_movs
 
+mono ./sim.exe listing_0041_add_sub_cmp_jnz > output/listing_0041_add_sub_cmp_jnz.asm
+nasm output/listing_0041_add_sub_cmp_jnz.asm
+diff -q output/listing_0041_add_sub_cmp_jnz listing_0041_add_sub_cmp_jnz
